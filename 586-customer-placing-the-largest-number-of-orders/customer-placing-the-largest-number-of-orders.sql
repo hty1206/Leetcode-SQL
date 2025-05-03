44d98bdf31c9f77e0edaaa55e05437181e0d51eb
Anita Hu <hty891206@gmail.com>
2025-05-03 01:57:16 -0500

    Time: 503 ms (61.46%) | Memory: 0B (100.00%) - LeetSync

--- 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
+++ 586-customer-placing-the-largest-number-of-orders/customer-placing-the-largest-number-of-orders.sql
@@ -2,11 +2,5 @@
 SELECT customer_number
 FROM Orders
 GROUP BY customer_number
-HAVING COUNT(customer_number) = (
-    SELECT MAX(order_count)
-    FROM (
-        SELECT COUNT(customer_number) AS order_count
-        FROM Orders
-        GROUP BY customer_number
-    ) AS subquery
-)
+ORDER BY COUNT(customer_number) DESC
+LIMIT 1
