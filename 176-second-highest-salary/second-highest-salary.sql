5c69df34417db384c6eb2dccce245be165812321
Anita Hu <hty891206@gmail.com>
2025-05-15 11:30:23 -0500

    Time: 280 ms (62.83%) | Memory: 0B (100.00%) - LeetSync

--- 176-second-highest-salary/second-highest-salary.sql
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -1,11 +1,9 @@
 # Write your MySQL query statement below
-SELECT CASE WHEN COUNT(id) = 1 THEN null
-ELSE (
-    SELECT salary
+SELECT CASE WHEN COUNT(DISTINCT(salary)) = 1 THEN null ELSE (
+    SELECT salary 
     FROM Employee
-    WHERE salary < (SELECT MAX(salary) FROM Employee)
+    GROUP BY salary
     ORDER BY salary DESC
-    LIMIT 1
-)
-END AS SecondHighestSalary
+    LIMIT 1 OFFSET 1
+) END AS SecondHighestSalary
 FROM Employee
