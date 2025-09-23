6d082cde674445651fcc9c856d3ed3a973e48b64
Anita Hu <hty891206@gmail.com>
2025-09-23 17:10:17 -0500

    Time: 593 ms (76.19%) | Memory: 0B (100.00%) - LeetSync

--- 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
+++ 2041-the-latest-login-in-2020/the-latest-login-in-2020.sql
@@ -1,5 +1,6 @@
 # Write your MySQL query statement below
-SELECT user_id, MAX(time_stamp) AS last_stamp
+SELECT user_id,
+MAX(time_stamp) AS last_stamp
 FROM Logins
-WHERE time_stamp LIKE '2020%' 
+WHERE time_stamp LIKE '2020%'
 GROUP BY user_id
