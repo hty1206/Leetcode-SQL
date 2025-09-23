5b55a92a13c15e6f6bdce68bbafea5042f62367c
Anita Hu <hty891206@gmail.com>
2025-09-23 17:05:47 -0500

    Time: 518 ms (94.92%) | Memory: 0B (100.00%) - LeetSync

--- 2024-calculate-special-bonus/calculate-special-bonus.sql
+++ 2024-calculate-special-bonus/calculate-special-bonus.sql
@@ -1,6 +1,5 @@
 # Write your MySQL query statement below
-SELECT employee_id, (
-    CASE WHEN name LIKE 'M%' OR (employee_id % 2 = 0) THEN 0 ELSE salary END
-) AS bonus
+SELECT employee_id,
+CASE WHEN (employee_id % 2 = 1 AND name NOT LIKE 'M%') THEN salary ELSE 0 END AS bonus
 FROM Employees
 ORDER BY employee_id
