9df30f9a79a2c404d3b8a8ffd74552e951edfea1
Anita Hu <hty891206@gmail.com>
2025-10-01 18:00:04 -0500

    Time: 314 ms (40.04%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -4,7 +4,7 @@ FROM (
     SELECT requester_id AS id FROM RequestAccepted
     UNION ALL
     SELECT accepter_id AS id FROM RequestAccepted
-) AS all_Accepted
+)AS all_RequestAccepted
 GROUP BY id
 ORDER BY num DESC
 LIMIT 1
