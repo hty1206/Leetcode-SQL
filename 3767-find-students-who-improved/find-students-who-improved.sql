60c03ac3b5a6748bade1d582e710dd7b39e7e615
Anita Hu <hty891206@gmail.com>
2025-10-06 18:02:35 -0500

    Time: 284 ms (68.77%) | Memory: 0B (100.00%) - LeetSync

--- 3767-find-students-who-improved/find-students-who-improved.sql
+++ 3767-find-students-who-improved/find-students-who-improved.sql
@@ -2,8 +2,9 @@
 SELECT S1.student_id, S1.subject, S1.score AS first_score, S2.score AS latest_score
 FROM Scores S1
 JOIN Scores S2
-ON S1.student_id = S2.student_id AND S1.subject = S2.subject AND S1.exam_date != S2.exam_date
+ON S1.student_id = S2.student_id AND S1.subject = S2.subject
+AND S1.exam_date != S2.exam_date
 AND S1.exam_date = (SELECT MIN(exam_date) FROM Scores WHERE S1.student_id = student_id AND S1.subject = subject)
 AND S2.exam_date = (SELECT MAX(exam_date) FROM Scores WHERE S2.student_id = student_id AND S2.subject = subject)
 WHERE S1.score < S2.score
-ORDER BY S1.student_id, S1.subject
+ORDER BY student_id, subject
